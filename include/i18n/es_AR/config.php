e59887f6aa7c80abff67a9c5b3a8d13d1165617b
geeky.ciudapolis@gmail.com <geeky.ciudapolis@gmail.com>
2025-09-30 01:03:11 -0500

    idioma

---
+++ include/i18n/es_AR/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'name' => 'Español (Argentina)',
+    'locale' => 'es_AR',
+    'date' => 'd/m/Y',
+    'date_time' => 'd/m/Y H:i',
+    'short_date' => 'd/m/y',
+    'datetime_format' => 'd/m/Y H:i',
+    'daydatetime_format' => 'd/m/Y H:i',
+);
+?>
---
+++ include/i18n/es_AR/phrases.php
@@ -0,0 +1,24 @@
+<?php
+return array(
+    'Email Address' => 'Dirección de Email',
+    'Password' => 'Contraseña',
+    'Log In' => 'Iniciar Sesión',
+    'Welcome' => 'Bienvenido',
+    'Home' => 'Inicio',
+    'Tickets' => 'Tickets',
+    'New Ticket' => 'Nuevo Ticket',
+    'My Tickets' => 'Mis Tickets',
+    'My Account' => 'Mi Cuenta',
+    'Log Out' => 'Cerrar Sesión',
+    'Create New Ticket' => 'Crear Nuevo Ticket',
+    'Check Ticket Status' => 'Ver Estado del Ticket',
+    'Subject' => 'Asunto',
+    'Message' => 'Mensaje',
+    'Submit' => 'Enviar',
+    'Name' => 'Nombre',
+    'Email' => 'Email',
+    'Help Topic' => 'Tema de Ayuda',
+    'Priority' => 'Prioridad',
+    'Attachments' => 'Archivos Adjuntos',
+);
+?>
